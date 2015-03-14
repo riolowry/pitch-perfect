f89e04c9c4ac4d47d5632aedbb821aa39effaa0d
Rio Lowry <macbookair@Rios-MacBook-Air.local>
2015-03-14 13:30:53 -0600

    Initial Commit

---
+++ Pitch Perfect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pitch Perfect
+//
+//  Created by Rio Lowry on 2015-03-14.
+//  Copyright (c) 2015 Rio Lowry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
